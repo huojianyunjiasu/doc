41b50c606ab5e8dda464f2fd1b7276af2aa6beb7
doc <huojianyunjiasu@gmail.com>
2022-10-25 20:14:52 +0800

    build

---
+++ dev/docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    title: '帮助中心',
+    description: 'Just playing around',
+    base:'/doc/',
+    themeConfig: {
+      sidebar: [
+        '/',
+        '/Category_1/',
+        {
+          title: 'Category2_nest',
+          path: '/Category_2/',
+          children: [
+            '/Category_2/test1.md',
+            '/Category_2/test2.md',
+          ]
+        }
+      ]
+    }
+  }
